2aff15034a4f3c84dacd49a4d745b310f4819798
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-10 22:35:03 -0600

    feat: add migrations check to data source initialization

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import 'reflect-metadata';
 import { AppDataSource } from '@/data-source';
 import type { DataSource } from 'typeorm';
 
-const main = async (_appDataSource: DataSource) => {};
+const main = async (appDataSource: DataSource) => {
+  console.log('🚀 App is Running');
+
+  const thereArePendingMigrations = await appDataSource.showMigrations();
+
+  if (thereArePendingMigrations) {
+    await appDataSource.runMigrations();
+  }
+};
 
 AppDataSource.initialize().then(main).catch(console.error);
