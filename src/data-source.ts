fbb8357e18636345ea21d28aa761b72542f7a144
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-10 18:24:15 -0600

    feat: add `App` data source

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+
+if (process.env.PORT == null) throw new Error('`PORT` is not defined');
+
+const parsedPort = parseInt(process.env.PORT);
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
+  port: parsedPort,
+  username: process.env.USERNAME,
+});
