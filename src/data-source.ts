f6f1d54396ed120f19a2022d205eed88986913bd
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-10 22:34:21 -0600

    feat: add `Client` entity to data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
+import { Client } from '@entity/Client';
+
 if (process.env.PORT == null) throw new Error('`PORT` is not defined');
 
 const parsedPort = parseInt(process.env.PORT);
@@ -8,6 +10,7 @@ const parsedPort = parseInt(process.env.PORT);
 export const AppDataSource = new DataSource({
   type: 'postgres',
   database: process.env.DATABASE,
+  entities: [Client],
   host: process.env.HOST,
   password: process.env.PASSWORD,
   port: parsedPort,
