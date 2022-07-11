8d143cd9e1e187754af7b00dc7dcded3b83d53fb
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-10 22:34:31 -0600

    feat: add `migrations` to data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,15 @@ if (process.env.PORT == null) throw new Error('`PORT` is not defined');
 
 const parsedPort = parseInt(process.env.PORT);
 
+const MIGRATIONS_DIR = `${__dirname}/migrations/**/*.ts`;
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   database: process.env.DATABASE,
   entities: [Client],
   host: process.env.HOST,
+  logging: true,
+  migrations: [MIGRATIONS_DIR],
   password: process.env.PASSWORD,
   port: parsedPort,
   username: process.env.USERNAME,
