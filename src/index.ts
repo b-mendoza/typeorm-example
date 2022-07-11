0045677e8c4723d8da0d843be93d2979e4b11364
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-10 18:24:33 -0600

    feat: add entry point

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+
+import { AppDataSource } from '@/data-source';
+import type { DataSource } from 'typeorm';
+
+const main = async (_appDataSource: DataSource) => {};
+
+AppDataSource.initialize().then(main).catch(console.error);
